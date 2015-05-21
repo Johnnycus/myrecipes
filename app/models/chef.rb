1d0f0e477c34acf854654923ac2c8033d23fcc7a
Albert Itaev <ijohnnyzzz@gmail.com>
2015-05-21 17:18:27 +0000

    create chef and recipes  association and tests

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,5 @@
 class Chef < ActiveRecord::Base
+  has_many :recipes
   before_save { self.email = email.downcase }
   validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
   VALID_EMAIL_REDEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,12 +3,18 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: "chicken parm", summary: "this is the best chicken parm recipe ever",
+    @chef = Chef.create(chefname: "bob", email: "bob@example.com")
+    @recipe = @chef.recipes.build(name: "chicken parm", summary: "this is the best chicken parm recipe ever",
               description: "heat all, add onions, add tomato sauce, add chicken, cook for 20 minutes")
   end
 
   test "recipe should be valid" do
     assert @recipe.valid?
+  end
+
+  test "chef_id should be present" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
 
   test "name should be present" do
