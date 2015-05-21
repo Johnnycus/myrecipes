40c0e1c801b06310ed00bf5c79992561a907f8eb
Albert Itaev <ijohnnyzzz@gmail.com>
2015-05-21 16:40:50 +0000

    add tests for chef model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,8 @@
 class Chef < ActiveRecord::Base
-
+  before_save { self.email = email.downcase }
+  validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
+  VALID_EMAIL_REDEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+                                    uniqueness: { case_sensitive: false },
+                                    format: { with: VALID_EMAIL_REDEX }
 end
