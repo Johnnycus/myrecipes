e1d417d828987921ee52f4bbc941cb313494189b
Albert Itaev <ijohnnyzzz@gmail.com>
2015-06-23 04:50:08 +0000

    Add register and edit profile features

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620093416) do
+ActiveRecord::Schema.define(version: 20150623042512) do
 
   create_table "chefs", force: true do |t|
     t.string   "chefname"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   create_table "likes", force: true do |t|
