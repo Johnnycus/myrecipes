15493252ce34604b6ebb1b17e16b97d8d107f467
Albert Itaev <ijohnnyzzz@gmail.com>
2015-07-01 07:33:31 +0000

    Add login and logout feature

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -12,6 +12,7 @@ class ChefsController < ApplicationController
     @chef = Chef.new(chef_params)
     if @chef.save
       flash[:success] = "Your account has been created successfully"
+      session[:chef_id] = @chef.id
       redirect_to recipes_path
     else
       render 'new'
